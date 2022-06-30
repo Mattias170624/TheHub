0f4266e8d0f5fbb94d70f603b4fcb49b01c13260
Mattias170624 <ExozTic_170624@outlook.com>
2022-07-01 01:15:40 +0200

    Added animation and transitions to navbar and welcome message. Moved variables to root css.

--- src/Components/Navigation/Navbar/Navbar.jsx
+++ src/Components/Navigation/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         <div id='navbar-Container'>
             <section id='navbar-Link-List'>
                 <NavLink to='/'>HOME</NavLink>
-                <img src={logo2} alt='Logo of webpage' id='navbar-Logo' className='fade-in-down' />
+                <img src={logo2} alt='Logo of webpage' id='navbar-Logo' className='slide-in-left' />
                 <NavLink to='/contact'>CONTACT</NavLink>
             </section>
         </div>
--- src/Components/Startscreen/Home.jsx
+++ src/Components/Startscreen/Home.jsx
@@ -2,9 +2,18 @@ import './home.css';
 
 const Home = () => {
     return (
-        <>
-            <p>Startscreen</p>
-        </>
+        <section id='header-Background'>
+            <div id='welcome-Container' className='fade-in-down'>
+                <section id='header-Text'>
+                    <h1>Hello You!</h1>
+                    <p>This website acts as a central hub for all my projects that are
+                        finished but also for those that are still in progress.
+                        <br /><br />
+                        You will find additional details when clicking on each project.
+                    </p>
+                </section>
+            </div>
+        </section>
     );
 }
 
