844a632aa588fba582c9b3cb12f6e50f5c2cfeba
Mattias170624 <ExozTic_170624@outlook.com>
2022-07-01 21:39:29 +0200

    Added title below homepage. Added searchbar (non functional). Updated global variables.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import ReactDOM from 'react-dom/client';
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import TitleBar from './Components/ProjectScreen/TitleBar';
 import Navbar from './Components/Navigation/Navbar/Navbar';
-import './App.css';
 import Home from './Components/Startscreen/Home';
+import './App.css';
 
 function App() {
   return (
     <HashRouter>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={
+          <>
+            <Home />
+            <TitleBar />
+          </>
+        } />
       </Routes>
     </HashRouter>
   );
