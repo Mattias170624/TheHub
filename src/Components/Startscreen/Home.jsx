32b761c21bd42ff10858488ebc730e6e90a65276
Mattias170624 <ExozTic_170624@outlook.com>
2022-07-15 15:46:10 +0200

    Changed header style and centered links. Moved some keyframe classes to their respective component css. Fixed project card overflowing.

--- src/Components/Startscreen/Home.jsx
+++ src/Components/Startscreen/Home.jsx
@@ -1,9 +1,11 @@
+import { BsChevronDown } from 'react-icons/bs';
+import { AiOutlineDoubleRight } from 'react-icons/ai';
 import './home.css';
-
 const Home = () => {
+
     return (
         <section id='header-Background'>
-            <div id='welcome-Container' className='fade-in-down'>
+            <div id='welcome-Container'>
                 <section id='header-Text'>
                     <h1>Hello You!</h1>
                     <p>This website acts as a central hub for all my projects that are
@@ -11,6 +13,8 @@ const Home = () => {
                         <br /><br />
                         You will find additional details when clicking on each project.
                     </p>
+
+                    <button id='socials-Button'>My socials <AiOutlineDoubleRight /></button>
                 </section>
             </div>
         </section>
