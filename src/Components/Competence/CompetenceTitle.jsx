8092a46e398327ab5a2fdb6be2433152bb8fbdd2
Mattias170624 <ExozTic_170624@outlook.com>
2023-05-12 00:28:15 +0200

    Added competence page

---
+++ src/Components/Competence/CompetenceTitle.jsx
@@ -0,0 +1,18 @@
+import "./CompetenceTitle.css";
+
+const CompetenceTitle = () => {
+  return (
+    <>
+      <section id="Comp-Title-Container">
+        <div id="Comp-Title-Wrapper">
+          <section>
+            <h3>Competence</h3>
+            <div id="Comp-Title-Line"></div>
+          </section>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default CompetenceTitle;
