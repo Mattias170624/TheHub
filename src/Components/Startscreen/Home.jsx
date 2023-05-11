eb8c9d1df9885bfba8660ccd9de93e6c07e9715d
Mattias170624 <ExozTic_170624@outlook.com>
2023-05-11 21:42:00 +0200

    Remade home screen

--- src/Components/Startscreen/Home.jsx
+++ src/Components/Startscreen/Home.jsx
@@ -1,21 +1,55 @@
-import './home.css';
-const Home = () => {
+import logo from "./../../Images/avatarLogo.png";
+import "./home.css";
 
-    return (
-        <section id='header-Background'>
-            <a id="home"></a>
-            <div id='welcome-Container'>
-                <section id='header-Text'>
-                    <h1>Hello You!</h1>
-                    <p>This website acts as a central hub for all my projects that are
-                        finished but also for those that are still in progress.
-                        <br /><br />
-                        You will find additional details when clicking on each project.
-                    </p>
-                </section>
-            </div>
-        </section>
-    );
-}
+const Home = () => {
+  return (
+    <section id="Header-Background">
+      <a id="home"></a>
+      <div id="Header-Container">
+        <div id="Header-Wrapper">
+          <section id="Mobile-Header-Logo-Container">
+            <img alt="My avatar" id="Mobile-Header-Logo" src={logo} />
+          </section>
+
+          <section id="Header-Text">
+            <h3>MATTIAS</h3>
+            <h3>ANDERSSON</h3>
+            <div id="Header-Line-1"></div>
+            <div id="Header-Line-2"></div>
+            <p>mobile developer</p>
+            <section id="Header-Social-Container">
+              <ul>
+                <li>
+                  <a href="https://www.linkedin.com/in/mattias-andersson-71bb481a0/" target="_blank">
+                    <img id="Social-Img-1" />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://github.com/Mattias170624" target="_blank">
+                    <img id="Social-Img-2" />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://discordapp.com/users/385882136533794819" target="_blank">
+                    <img id="Social-Img-3" />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.figma.com/@mattias170624" target="_blank">
+                    <img id="Social-Img-4" />
+                  </a>
+                </li>
+              </ul>
+            </section>
+          </section>
+
+          <section>
+            <img alt="My avatar" id="Header-Logo" src={logo} />
+          </section>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Home;
