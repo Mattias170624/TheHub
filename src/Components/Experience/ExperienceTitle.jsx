f0f9621464931f8a229020991b3b1402325bbb89
Mattias170624 <ExozTic_170624@outlook.com>
2023-05-13 18:04:51 +0200

    Added experience page

---
+++ src/Components/Experience/ExperienceTitle.jsx
@@ -0,0 +1,18 @@
+import "./ExperienceTitle.css";
+
+const ExperienceTitle = () => {
+  return (
+    <>
+      <section id="Exp-Title-Container">
+        <div id="Exp-Title-Wrapper">
+          <section>
+            <h3>Experiences</h3>
+            <div id="Exp-Title-Line"></div>
+          </section>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default ExperienceTitle;
