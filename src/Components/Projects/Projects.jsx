dd9d00ae013ff99d69262cb8cc2833cb0bd0e25f
Mattias170624 <ExozTic_170624@outlook.com>
2023-05-16 20:14:42 +0200

    Add firebase connection

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,54 +1,89 @@
 import "./Projects.css";
 import icon from "./../../Images/link-Icon.png";
-import css from "./../../Images/css.png";
-import html from "./../../Images/html.png";
-import js from "./../../Images/JavaScript.png";
-import react from "./../../Images/React.png";
-import kotlin from "./../../Images/Kotlin.png";
-import swift from "./../../Images/Swift.png";
-import dart from "./../../Images/Dart.png";
-import firebase from "./../../Images/Firebase.png";
-import mongodb from "./../../Images/Mongodb.png";
+import Css from "./../../Images/css.png";
+import Html from "./../../Images/html.png";
+import Js from "./../../Images/JavaScript.png";
+import React from "./../../Images/React.png";
+import Kotlin from "./../../Images/Kotlin.png";
+import Swift from "./../../Images/Swift.png";
+import Dart from "./../../Images/Dart.png";
+import Firebase from "./../../Images/Firebase.png";
+import Mongodb from "./../../Images/Mongodb.png";
+import { useEffect, useState } from "react";
+import { getMobileProjects, getWebProjects } from "../../Api/projectsDataApi";
 
 const Projects = () => {
+  const [mobileProjects, setMobileProjects] = useState([]);
+  const [webProjects, setWebProjects] = useState([]);
+  const [otherProjects, setOtherProjects] = useState([]);
+
+  useEffect(() => {
+    callProjectApi();
+  }, []);
+
+  async function callProjectApi() {
+    setMobileProjects(await getMobileProjects());
+    setWebProjects(await getWebProjects());
+  }
+
   return (
     <>
       <section id="Projects-Container">
         <div id="Projects-Wrapper">
           <h2 className="Project-Title-Text">Mobile platform</h2>
-          <ProjectCard type="Mobile" />
+          <ProjectCard projectData={mobileProjects} />
+          <h2 className="Project-Title-Text">Web platform</h2>
+          <ProjectCard projectData={webProjects} />
+          <h2 className="Project-Title-Text">Other platform</h2>
+          <ProjectCard projectData={otherProjects} />
         </div>
       </section>
     </>
   );
 };
 
-function ProjectCard({ type }) {
-  return (
-    <div className="Project-Cards-Row">
-      {/* forEach project in ProjectMobile...*/}
-      <section className="Project-Card-Container">
-        <div className="Project-Card-Top">
-          <h3>Rent Flix</h3>
-          <p>
-            orem Ipsum has been the induummy text ever s efefefe fef ef e ince
-            the 150 ummy text ever s
-          </p>
-          <a href="https://www.google.com" target="_blank">
-            <img className="Project-Card-Link-Icon" src={icon} />
-          </a>
-        </div>
+function ProjectCard({ projectData }) {
+  if (projectData.length == 0) {
+    console.log("show loading image...");
+  } else {
+    return (
+      <div className="Project-Cards-Row">
+        {projectData.map((data) => (
+          <section className="Project-Card-Container">
+            <div className="Project-Card-Top">
+              <h3>{data.title}</h3>
+              <p>{data.desc}</p>
+              <a href={data.link} target="_blank">
+                <img className="Project-Card-Link-Icon" src={icon} />
+              </a>
+            </div>
+            <div className="Project-Card-Bottom">
+              <ProjectImages imageArray={data.icons} />
+            </div>
+          </section>
+        ))}
+      </div>
+    );
+  }
+}
 
-        <div className="Project-Card-Bottom">
-          {/* forEach project in ProjectMobile... images index instead of css...*/}
-          <img src={css} />
-          <img src={css} />
-          <img src={css} />
-          <img src={css} />
-        </div>
-      </section>
-    </div>
-  );
+function ProjectImages({ imageArray }) {
+  var imageSrcArray = [];
+
+  // Link to images based on what the imageArray contains
+  if (imageArray.includes("react")) imageSrcArray.push(React);
+  if (imageArray.includes("js")) imageSrcArray.push(Js);
+  if (imageArray.includes("html")) imageSrcArray.push(Html);
+  if (imageArray.includes("css")) imageSrcArray.push(Css);
+  if (imageArray.includes("dart")) imageSrcArray.push(Dart);
+  if (imageArray.includes("kotlin")) imageSrcArray.push(Kotlin);
+  if (imageArray.includes("swift")) imageSrcArray.push(Swift);
+  if (imageArray.includes("firebase")) imageSrcArray.push(Firebase);
+  if (imageArray.includes("mongodb")) imageSrcArray.push(Mongodb);
+
+  return imageSrcArray.map((src) => (
+    <img src={src} className="Project-Card-Images" />
+  ));
 }
 
 export default Projects;
--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,23 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBeXPPszgh-wHCFoepDiWDc2cO9abh92ss",
-  authDomain: "thehub-522b5.firebaseapp.com",
-  projectId: "thehub-522b5",
-  storageBucket: "thehub-522b5.appspot.com",
-  messagingSenderId: "42299940522",
-  appId: "1:42299940522:web:c11941b90ca164ad5abad3"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID 
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
